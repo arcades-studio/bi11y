7a1cca60ce76e75c608693f4abf10aeb173eeff9
Jake Brussel Faria <jake@jakebf.com>
2021-08-10 18:05:29 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,64 @@
+const prod = process.env.NODE_ENV === "production";
+const Image = require("@11ty/eleventy-img");
+let Postcss = require('postcss')
+let postcssPlugins = [
+  require('postcss-preset-env')(),
+  require('cssnano')({
+      preset: 'default',
+  }),
+]
+
+async function imageShortcode(src, alt, sizes, classes) {
+  let metadata = await Image(src, {
+    widths : prod ? [200, 400, 600, 800, 1000, 1400, 1600] : [1000, 1400, 1600],
+    formats: prod ? ["webp", "jpeg"] : ["webp"],
+    outputDir: './_site/img',
+  });
+
+  let imageAttributes = {
+    class: classes,
+    alt,
+    sizes,
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src");
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+
+  eleventyConfig.addNunjucksAsyncFilter("postcss", async function (
+    css,
+    callback
+  ) {
+    
+    try {
+      const processedCss = await Postcss(postcssPlugins).process(css, { from: undefined });
+      callback(null, processedCss);
+    } catch (err) {
+      console.error("Postcss error: ", err);
+      callback(null, css);
+    }
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    "public": "/"
+  });
+
+  eleventyConfig.on('afterBuild', () => {
+    require('esbuild').buildSync({
+      entryPoints: ['./scripts/index.js'],
+      bundle: true,
+      minify: prod,
+      outfile: '_site/index.js',
+    })
+  });
+
+  return {
+    dir: {
+      input: "pages",
+      includes: "../components",
+    }
+  }
+};
